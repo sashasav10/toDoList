304aebcf31c4b039441b15dc8fc5cf74b7c01a9d
rhavuka148 <rostyslav.havuka@ekreative.com>
2023-01-16 14:46:07 +0200

    fix provider

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,6 @@ import 'detailed_task_screen.dart';
 
 void main() => runApp(const TodoApp());
 
-final GoRouter _router = GoRouter(
-  initialLocation: '/',
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const TodoList();
-      },
-    ),
-    GoRoute(
-      path: '/detailed_task_screen',
-      builder: (BuildContext context, GoRouterState state) {
-        //  String index = state.extra as String;
-        return DetailedTaskScreen(id: "0");
-      },
-    ),
-  ],
-);
-
 class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
 
@@ -38,22 +19,35 @@ class TodoApp extends StatelessWidget {
       providers: [
         Provider<Uuid>(create: (context) => const Uuid()),
         Provider<TodoDbService>(create: (context) => TodoDbService()),
-        Provider<TodoStore>(
-          create: (context) => TodoStore(
-            uuid: Provider.of<Uuid>(context, listen: false),
-            todoDbService: TodoDbService.of(context),
-          ),
-        ),
       ],
-      child: Builder(
-        builder: (context) {
-          return MaterialApp.router(
-            routerConfig: _router,
-            theme: ThemeData(
-              primarySwatch: Colors.red,
+      builder: (context, child) => MaterialApp.router(
+        routerConfig: GoRouter(
+          initialLocation: '/',
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/',
+              builder: (BuildContext context, GoRouterState state) {
+                return Provider(
+                  create: (context) => TodoStore(
+                    uuid: Provider.of<Uuid>(context, listen: false),
+                    todoDbService: TodoDbService.of(context),
+                  ),
+                  child: const TodoList(),
+                );
+              },
             ),
-          );
-        },
+            GoRoute(
+              path: '/detailed_task_screen',
+              builder: (BuildContext context, GoRouterState state) {
+                //  String index = state.extra as String;
+                return DetailedTaskScreen(id: "0");
+              },
+            ),
+          ],
+        ),
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
       ),
     );
   }
--- lib/todo_db_service.dart
+++ lib/todo_db_service.dart
@@ -15,7 +15,10 @@ class TodoDbService {
   Future<List<Todo>> getTodoFromSF() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? todoString = prefs.getString(_todoKey);
-    return await decode(todoString!);
+
+    ///TODO
+    if (todoString == null) return [];
+    return await decode(todoString);
   }
 
   String encode(List<Todo> todos) => json.encode(
